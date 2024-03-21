e4163ebdf0193232ef5b3ccd4520a6ac3817390e
INIF-FISH <iratecat1@outlook.com>
2024-03-21 20:58:07 +0800

    update

--- scripts/WatchDog_full system.sh
+++ scripts/WatchDog_full system.sh
@@ -6,15 +6,16 @@ declare -a commands=(
   "ros2 launch ig_lio_c map_mapping_launch.py"
   "ros2 launch livox_ros_driver2 msg_MID360_launch.py"
   "ros2 launch nav2_bringup base_robot_launch.py"
-	"ros2 launch sentry_decision_v2 launch.py"
-	"ros2 run rm_serial_port rm_serial_port_node"
+  "ros2 launch sentry_decision_v2 launch.py"
+  "ros2 launch armor_processor autoaim_bringup_launch.py"
   )
 
 # 定义函数来启动ros命令
 start_commands() {
+  echo "sys666" | sudo -S  ifconfig lo multicast
   for cmd in "${commands[@]}"
   do
-	gnome-terminal -- bash -c "  source /opt/ros/galactic/setup.bash;cd $(pwd);source install/setup.bash;$cmd;exec bash;"
+	gnome-terminal -- bash -c " cd $(pwd);source install/setup.bash;$cmd;exec bash;"
   done
 }
 
@@ -29,7 +30,7 @@ watch_commands() {
       if ! pgrep -f "$cmd" > /dev/null
       then
         echo "$cmd 未在运行, 重新启动"
-	gnome-terminal -- bash -c "  source /opt/ros/galactic/setup.bash;cd $(pwd);source install/setup.bash;$cmd;exec bash;"
+	gnome-terminal -- bash -c "  cd $(pwd);source install/setup.bash;$cmd;exec bash;"
       fi
     done
     sleep 10 # 设置睡眠时间，以便在下一次检查之前给进程足够的时间运行
